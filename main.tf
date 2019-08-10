3f6481ca39bab44203d8c7c65a39d02878d8be5e
brennaman <paulbrennaman@outlook.com>
2019-08-10 17:49:33 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  environment                   = "${var.azure_environment}"
+  subscription_id               = "${var.azure_subscription_id}"
+  client_id                     = "${var.azure_client_id}"
+  client_certificate_path       = "${var.client_certificate_path}"
+  client_certificate_password   = "${var.client_certificate_password}"
+  tenant_id                     = "${var.azure_tenant_id}"
+}
+
+terraform {
+  backend "azurerm" {}
+}
